60d8480e48536829818c2f265eafd8e3afbcec6f
Mirfozil <mirfoziln@gmail.com>
2023-07-12 15:29:35 +0500

    add middleware

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logEvents");
+const errHandler = require("./middleware/errHandler");
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+const whiteList = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if(whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    }else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
   // res.sendFile('./views/index.html', {root: __dirname});
@@ -39,13 +63,22 @@ const two = (req, res, next) => {
 
 const three = (req, res, next) => {
   console.log("three");
-  res.send('finished !!!')
+  res.send("finished !!!");
 };
 
-app.get('/chain(.html)?', [one, two, three]);
+app.get("/chain(.html)?", [one, two, three]);
 
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if(req.accepts('html')) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  }else if(req.accepts('json')) {
+    res.json({error: '404 not found'})
+  }else {
+    res.type('txt').send('404 not found')
+  }
 });
+
+app.use(errHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
